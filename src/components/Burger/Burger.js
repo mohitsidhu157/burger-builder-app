3d6b67ef4e78de33944f2f9d06d21c9941cfd289
Mohit kumar <mohitsidhu157@gmail.com>
2019-07-20 12:20:24 +0530

    Reducing ingredients array of arrays to a single array in burger.js

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,16 @@ import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredient";
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_,i)=>{
                 return <BurgerIngredient key={igKey + i} type={igKey}/>;
             });
-        });
+        }).reduce((arr,curEl)=>arr.concat(curEl),[]);
+    console.log(transformedIngredients);
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients !</p>
+    }
     return <div className={classes.Burger}>
                 <BurgerIngredient type="bread-top"/>
                 {transformedIngredients}
