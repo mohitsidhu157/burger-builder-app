c7485df059619d1dbc3dcc25ff69dc71db8311ac
Mohit kumar <mohitsidhu157@gmail.com>
2019-07-25 20:07:09 +0530

    Improving performance by updating OrdeSummary only when it is shun to user

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from "../../../hoc/Aux";
 import Button from "../../UI/Button/Button";
-const orderSummary = (props)=>{
-    const ingredientSummary = Object.keys(props.ingredients)
-                              .map(igKey =>{
-                                  return <li key={igKey}>
-                                            <span style={{textTransform : 'capitalize'}}>{igKey}</span> : {props.ingredients[igKey]}
-                                        </li>
-                              }) 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the folowing ingredients : </p>
-            <ul>{ingredientSummary}</ul>
-            <p><strong>Total price : {props.price}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType="Danger" clicked={props.cancel}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.continue}>CONTINUE</Button>
-        </Aux>
-    );
+class OrderSummary extends Component{
+    componentWillUpdate(){
+        console.log("[OrderSummary] willUpdate");
+    }
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igKey =>{
+            return <li key={igKey}>
+                      <span style={{textTransform : 'capitalize'}}>{igKey}</span> : {this.props.ingredients[igKey]}
+                  </li>
+        }) 
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the folowing ingredients : </p>
+                <ul>{ingredientSummary}</ul>
+                <p><strong>Total price : {this.props.price}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType="Danger" clicked={this.props.cancel}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.continue}>CONTINUE</Button>
+            </Aux>
+        );
+    }
 }
-export default orderSummary;
+export default OrderSummary;
